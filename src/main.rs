3a2682db0cbd3bf2f12b3b0e3205d5f620e6c35e
Toxumuharu <ring.o.awlright@gmail.com>
2021-01-06 22:06:01 +0900

    add function sorts array elements from smaller to larger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-// use ferris_says::say; // from the previous step
-// use std::io::{stdout, BufWriter};
+#[allow(unused_imports)]
 use rand::{thread_rng, Rng};
 
 fn main() {
-    const array_size: usize = 10;
-    let mut array1: [i32; array_size] = [-1; array_size];
-    let mut array2: [i32; array_size] = [-1; array_size];
+    const ARRAY_SIZE: usize = 10;
+    const ARRAY_RANGE_MIN: i32 = 0;
+    const ARRAY_RANGE_MAX: i32 = 100;
+    let mut array1: [i32; ARRAY_SIZE] = [-1; ARRAY_SIZE];
     let mut rng = thread_rng();
 
-    for i in 0..array_size {
-        array1[i] = rng.gen();
+    for i in 0..ARRAY_SIZE {
+        array1[i] = rng.gen_range(ARRAY_RANGE_MIN, ARRAY_RANGE_MAX);
     }
+    println!("{:?}", array1);
 
     // let stdout = stdout();
     // let message = String::from("Hello fellow Rustaceans!");
@@ -19,14 +20,19 @@ fn main() {
     // let mut writer = BufWriter::new(stdout.lock());
     // say(message.as_bytes(), width, &mut writer).unwrap();
 
-    sort_array(&mut array1, &mut array2);
+    sort_array(&mut array1);
+    println!("{:?}", array1);
 }
 
-fn sort_array(array1: &mut [i32], array2: &mut [i32]){
-    for i in array1.iter() {
-        println!("{}", i);
-        for j in array1.iter() {
-            print!(" {}",j)
+fn sort_array(array1: &mut [i32]){
+    let mut temp: i32;
+    for i in 0..array1.len() {
+        for j in 0..array1.len() {
+            if array1[i] < array1[j] {
+                temp = array1[i];
+                array1[i] = array1[j];
+                array1[j] = temp;
+          }
         }
     } 
 }
